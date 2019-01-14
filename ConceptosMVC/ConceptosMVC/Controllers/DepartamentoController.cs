5c761394f789379aa18c829c4578c50df9d29100
David <davidvalencia_@hotmail.com>
2019-01-14 14:22:33 +0100

    19.1.14.14

---
+++ ConceptosMVC/ConceptosMVC/Controllers/DepartamentoController.cs
@@ -0,0 +1,48 @@
+﻿using ConceptosMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConceptosMVC.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        HelperDepartamento helper;
+
+        public DepartamentoController()
+        {
+            helper = new HelperDepartamento();
+        }
+
+        // GET: Departamento
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CrearDepartamento()
+        {
+            DEPT d = new DEPT();
+            d.DEPT_NO = this.helper.GetMaxIdDepartamento();
+            return View(d);
+        }
+        
+        [HttpPost]
+        public ActionResult CrearDepartamento(DEPT departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                this.helper.InsertarDepartamento(departamento);
+
+
+                return View("Index");
+            }
+
+            DEPT d = new DEPT();
+            d.DEPT_NO = this.helper.GetMaxIdDepartamento();
+            return View(d);
+        }
+    }
+}
---
+++ ConceptosMVC/ConceptosMVC/Models/HelperDepartamento.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConceptosMVC.Models
+{
+    public class HelperDepartamento
+    {
+        EntidadHospital entity;
+        public HelperDepartamento()
+        {
+            entity = new EntidadHospital();
+        }
+
+        public int GetMaxIdDepartamento()
+        {
+            int numero = (int)this.entity.GETMAXDEPTNO().FirstOrDefault();
+            numero+= 10;
+            return numero;
+        }
+
+        public void InsertarDepartamento(DEPT departamento)
+        {
+            entity.DEPT.Add(departamento);
+            entity.SaveChanges();
+        }
+
+        public List<DEPT> GetDepartamentos()
+        {
+            var sql = entity.GETDEPT();
+            List<DEPT> depts = sql.ToList();
+            return depts;
+        }
+    }
+}
